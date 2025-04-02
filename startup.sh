272412d11eb3b49680b58abc52d7fd8862c7c051
sebranchett <s.e.branchett@tudelft.nl>
2025-04-02 12:50:19 +0200

    connect to internet first

--- startup.sh
+++ startup.sh
@@ -4,6 +4,10 @@ set -ex
 # Set up paths and networks
 source .env.local
 
+# Connect to internet
+netsh wlan connect ssid=${SSID_INTERNET} name=${SSID_INTERNET_PROFILE}  || exit 1  # connect to internet
+sleep 5  # wait for the connection to be established
+
 cd $START_DIR/mirte-in-the-loop || exit 1
 
 git checkout $BRANCH || exit 1
