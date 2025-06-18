e6e15dc774312a8b3c3d982d192fe7e847fe34f9
sebranchett <s.e.branchett@tudelft.nl>
2025-06-18 11:06:11 +0200

    connect to wifi from linux as well

--- helper_scripts/connect_to_wifi.sh
+++ helper_scripts/connect_to_wifi.sh
@@ -7,12 +7,28 @@
 ssid=$1
 ssid_profile=$2
 
-netsh wlan disconnect
-netsh wlan connect ssid=${ssid} name=${ssid_profile}  || exit 1  # connect to internet
+# if the command nmcli is available (linux), use it to connect to the network
+if command -v nmcli &> /dev/null; then
+    sudo nmcli c up ${ssid} || exit 1
 
-# Check if connected to the network we asked for
-sleep 10 # wait for the connection to be established and stable
-network=$(netsh wlan show interfaces | grep ' SSID' |  grep ${ssid} | wc -l)
-if [ "$network" -eq 0 ]; then
+    # Check if connected to the network we asked for
+    sleep 10 # wait for the connection to be established and stable
+    network=$(nmcli device wifi list | grep -E '^\*' | grep ${ssid} | wc -l)
+    if [ "$network" -eq 0 ]; then
+        exit 1
+    fi
+# otherwise, try netsh (Windows)
+elif command -v netsh &> /dev/null; then
+    netsh wlan disconnect
+    netsh wlan connect ssid=${ssid} name=${ssid_profile}  || exit 1  # connect to internet
+
+    # Check if connected to the network we asked for
+    sleep 10 # wait for the connection to be established and stable
+    network=$(netsh wlan show interfaces | grep ' SSID' |  grep ${ssid} | wc -l)
+    if [ "$network" -eq 0 ]; then
+        exit 1
+    fi
+else
+    echo "Neither nmcli nor netsh is available. Cannot connect to WiFi."
     exit 1
 fi
